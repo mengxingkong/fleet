1545188f70f0b665b74fe90023cd12a416b2abc5
p_ljzzhang(张利军) <p_ljzzhang@tencent.com>
2019-01-25 17:51:57 +0800

    websocket

---
+++ src/main/java/com/warren/fleet/websocket/interceptor/WebsocketInterceptor.java
@@ -0,0 +1,30 @@
+package com.warren.fleet.websocket.interceptor;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+public class WebsocketInterceptor implements HandshakeInterceptor {
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Map<String, Object> map) throws Exception {
+
+        //可以在这里进行 token 的验证
+        if (serverHttpRequest instanceof ServletServerHttpRequest) {
+            ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) serverHttpRequest;
+            HttpSession session = servletRequest.getServletRequest().getSession();
+            map.put("sessionId", session.getId());
+        }
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Exception e) {
+
+    }
+}
