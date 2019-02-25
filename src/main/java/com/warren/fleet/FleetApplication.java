abb2e08fa9020f773003f48a21a1cc79c217aa9b
p_ljzzhang(张利军) <p_ljzzhang@tencent.com>
2019-02-25 15:57:50 +0800

    flowable config

--- src/main/java/com/warren/fleet/FleetApplication.java
+++ src/main/java/com/warren/fleet/FleetApplication.java
@@ -1,10 +1,15 @@
 package com.warren.fleet;
 
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {
@@ -15,4 +20,22 @@ public class FleetApplication {
     public static void main(String[] args) {
         SpringApplication.run(FleetApplication.class, args);
     }
+
+//    @Bean
+//    public CommandLineRunner init(final RepositoryService repositoryService,
+//                                  final RuntimeService runtimeService,
+//                                  final TaskService taskService) {
+//
+//        return new CommandLineRunner() {
+//            @Override
+//            public void run(String... strings) throws Exception {
+//                System.out.println("Number of process definitions : "
+//                        + repositoryService.createProcessDefinitionQuery().count());
+//                System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
+//                runtimeService.startProcessInstanceByKey("oneTaskProcess");
+//                System.out.println("Number of tasks after process start: "
+//                        + taskService.createTaskQuery().count());
+//            }
+//        };
+//    }
 }
--- src/main/java/com/warren/fleet/flowable/config/FlowableConfigration.java
+++ src/main/java/com/warren/fleet/flowable/config/FlowableConfigration.java
@@ -8,6 +8,7 @@ import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
 import org.flowable.spring.SpringProcessEngineConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -23,7 +24,6 @@ public class FlowableConfigration {
 
 
 //    @Bean
-//    @Primary
 //    public SpringProcessEngineConfiguration springProcessEngineConfiguration() {
 //        SpringProcessEngineConfiguration springProcessEngineConfiguration = new SpringProcessEngineConfiguration();
 //        springProcessEngineConfiguration.setDataSource(dataSource);
@@ -31,19 +31,19 @@ public class FlowableConfigration {
 //        return springProcessEngineConfiguration;
 //    }
 
-    @Bean
-    @Primary
-    protected ProcessEngineConfiguration configuration() {
-        ProcessEngineConfiguration processEngineConfiguration = StandaloneProcessEngineConfiguration
-                .createStandaloneProcessEngineConfiguration()
-                .setDataSource(dataSource);
-        return processEngineConfiguration;
-    }
-
-    @Bean
-    protected ProcessEngine engine() {
-        //创建流程引擎  服务启动是初始化一次即可
-        return ProcessEngines.getDefaultProcessEngine();
-    }
+//    @Bean
+//    @Primary
+//    protected ProcessEngineConfiguration configuration() {
+//        ProcessEngineConfiguration processEngineConfiguration = StandaloneProcessEngineConfiguration
+//                .createStandaloneProcessEngineConfiguration()
+//                .setDataSource(dataSource);
+//        return processEngineConfiguration;
+//    }
+//
+//    @Bean
+//    protected ProcessEngine engine() {
+//        //创建流程引擎  服务启动是初始化一次即可
+//        return ProcessEngines.getDefaultProcessEngine();
+//    }
 
 }
